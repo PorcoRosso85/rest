7a28df0bc375928ab501391a5f688393abe6b985
PorcoRosso85 <1.is.universe@gmail.com>
2024-02-13 17:38:08 +0900

    テスト構造を追加し、pytest-djangoをインストールしました

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Structure(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+import pytest
+
+
+@pytest.fixture
+def structure():
+    return Structure.objects.create(
+        name="Test Structure",
+        description="Test Description",
+    )
+
+
+@pytest.mark.django_db
+def test_structure(structure):
+    assert structure.name == "Test Structure"
+    assert structure.description == "Test Description"
+    assert structure.created_at
+    assert structure.updated_at
--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app",
-    "django_rich",
+    "pytest_django",
     "rest_framework",
+    "app",
 ]
 
 TEST_RUNNER = "django_rich.test.RichRunner"
@@ -132,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+PYTEST_ADDOPTS = "--disable-pytest-warnings"
