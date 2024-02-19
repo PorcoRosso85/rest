d3937d9730907bbe061bdef35a1183efb74fe2f8
PorcoRosso85 <1.is.universe@gmail.com>
2024-02-19 19:39:11 +0900

    app/signals/__init__.pyとapp/signals/data_model.pyを追加しました。データモデルのpre_saveシグナルを実装しました。テストも追加しました。publishment_state_model.pyとtest_publishment_state_model.pyも追加しました。publishment_statusのpost_saveシグナルを実装しました。テストも追加しました。

--- app/signals/publishment_state_model.py
+++ app/signals/publishment_state_model.py
@@ -1,5 +1,5 @@
 import pytest
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
@@ -38,41 +38,3 @@ class TestPostSaveSignalPublishmentStatus:
         """バリデーションが失敗する場合"""
         with pytest.raises(Exception):
             PublishmentStatus(status="").save()
-
-
-@receiver(pre_save, sender=Data)
-def presave_signal_data(sender, instance, **kwargs):
-    # craeted_atを追加する
-    if instance._created_at is None:
-        instance._created_at = timezone.now()
-    logger.info("data is valid")
-
-    # valueが空の場合はエラー
-    if instance.value == {}:
-        raise Exception("value is required")
-    logger.info("data is valid")
-
-    # updated_atを更新する
-    instance._updated_at = timezone.now()
-
-
-class TestPreSaveSignalData:
-    @pytest.fixture
-    def fixture_data(self):
-        return Data.objects.create(value={"key": "value"})
-
-    @pytest.mark.django_db
-    def test_正常系_日付が追加された(self, fixture_data):
-        assert fixture_data._created_at is not None
-        assert fixture_data._updated_at is not None
-
-    @pytest.mark.django_db
-    def test_正常系_バリデーションが成功する場合(self, fixture_data):
-        """バリデーションが成功する場合"""
-        assert fixture_data.value == {"key": "value"}
-
-    @pytest.mark.django_db
-    def test_異常系_バリデーションが失敗する場合(self):
-        """バリデーションが失敗する場合"""
-        with pytest.raises(Exception):
-            Data(value={}).save()
