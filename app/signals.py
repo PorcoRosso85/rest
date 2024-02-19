fb3ad8bec2c834660be76eefe1a14c27920da268
PorcoRosso85 <1.is.universe@gmail.com>
2024-02-19 12:30:24 +0900

    signals.pyの変更を追加***

--- app/signals.py
+++ app/signals.py
@@ -1,28 +1,56 @@
 import pytest
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.utils import timezone
 
-from app.models import PublishmentStatus
+from app.models import Data, PublishmentStatus
 from app.utils import logger
 
 
-@receiver(pre_save, sender=PublishmentStatus)
-def handler_publishment_status(sender, instance, **kwargs):
+@receiver(post_save, sender=PublishmentStatus)
+def postsave_signal_publishmentstate(sender, instance, **kwargs):
     if instance.status == "":
         raise Exception("status is required")
     logger.info("publishment status is valid")
 
+    # ここでpublishment_statusがpublishedの場合は、Dataインスタンスのpublished_atを追加する
+    if (
+        instance.status == "published"
+        and instance.status != instance.__class__.objects.get(pk=instance.pk).status
+    ):
+        instance.data._published_at = timezone.now()
+        instance.data.save()
+        logger.info("published_at is added")
 
-@pytest.mark.django_db
-def test_正常系_バリデーションが成功する場合():
-    """バリデーションが成功する場合"""
-    status = PublishmentStatus(status="published")
-    status.save()
-    assert status.status == "published"
+
+class TestPublishmentStatusSignal:
+    @pytest.mark.django_db
+    def test_正常系_バリデーションが成功する場合(self):
+        """バリデーションが成功する場合"""
+        value = {"key": "value"}
+        data = Data.objects.create(value=value)
+        status = PublishmentStatus.objects.create(data=data, status="published")
+        status.save()
+        assert status.status == "published"
+
+    @pytest.mark.django_db
+    def test_異常系_バリデーションが失敗する場合(self):
+        """バリデーションが失敗する場合"""
+        with pytest.raises(Exception):
+            PublishmentStatus(status="").save()
 
 
-@pytest.mark.django_db
-def test_異常系_バリデーションが失敗する場合():
-    """バリデーションが失敗する場合"""
-    with pytest.raises(Exception):
-        PublishmentStatus(status="").save()
+@receiver(pre_save, sender=Data)
+def presave_signal_data(sender, instance, **kwargs):
+    # craeted_atを追加する
+    if instance._created_at is None:
+        instance._created_at = timezone.now()
+    logger.info("data is valid")
+
+    # valueが空の場合はエラー
+    if instance.value == {}:
+        raise Exception("value is required")
+    logger.info("data is valid")
+
+    # updated_atを更新する
+    instance._updated_at = timezone.now()
