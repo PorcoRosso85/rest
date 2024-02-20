97c356c0c4f5e25d8a773bf00e5f707a240c3d48
PorcoRosso85 <1.is.universe@gmail.com>
2024-02-20 17:08:20 +0900

    ***削除されたファイルとマイグレーションの変更***
    ***変更内容:***
    - ファイル `0001.erd` が削除されました。
    - ファイル `__init__.py` が削除されました。
    - ファイル `0002.sql` の一部のフィールドが変更されました。
    - ファイル `0005_data__model.py` が削除されました。
    - ファイル `0002_alter_apikeys_key.py` が削除されました。
    - ファイル `0003_remove_user_associate_associate_user.py` が削除されました。
    - ファイル `0004_rename_key_apikeys_api_key_remove_apikeys_space_and_more.py` が削除されました。

--- app/migrations/0002.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
---
--- Alter field key on apikeys
---
-ALTER TABLE "app_apikeys" ADD CONSTRAINT "app_apikeys_key_428076af_uniq" UNIQUE ("key");
-CREATE INDEX "app_apikeys_key_428076af_like" ON "app_apikeys" ("key" varchar_pattern_ops);
-COMMIT;
