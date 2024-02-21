39aa787229e03952a4a54e09b6651826a4798618
PorcoRosso85 <1.is.universe@gmail.com>
2024-02-21 12:56:28 +0900

    Add Post and Comment models and serializers***
    ***ポストとコメントのモデルとシリアライザを追加

--- app/models.py
+++ app/models.py
@@ -142,3 +142,90 @@ class PublishmentStatus(models.Model):
         default=get_default_data,  # type: ignore
     )
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+import pytest
+from rest_framework import serializers
+
+from app.models import Post
+from app.utils import logger
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "text", "created_at"]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ["id", "title", "content", "comments"]
+
+    # []fixme validated_dataにidが渡されない
+    def update(self, instance: Post, validated_data) -> Post:
+        logger.debug("")
+        comments_data = validated_data.pop("comments", [])
+        for comment_data in comments_data:
+            logger.debug(f"### comment_data: {comment_data}")
+            comment_id = comment_data.get("id", None)
+            if comment_id:
+                # 既存のコメントを更新
+                comment = Comment.objects.get(id=comment_id, post=instance)
+                for attr, value in comment_data.items():
+                    setattr(comment, attr, value)
+                comment.save()
+            else:
+                # 新しいコメントを作成
+                Comment.objects.create(post=instance, **comment_data)
+
+        # Postインスタンスの更新
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
+
+
+class TestPostSerializer:
+    @pytest.mark.skip("updateメソッドがid: comment.idを受け取れないためスキップ")
+    @pytest.mark.django_db
+    def test_正常系_PostSerializerからコメントを更新できる(self) -> None:
+        post = Post.objects.create(title="title", content="content")
+        comment = Comment.objects.create(post=post, text="text")
+
+        serializer = PostSerializer(
+            instance=post,
+            data={
+                "title": "new title",
+                "content": "new content",
+                "comments": [
+                    {"id": comment.id, "text": "new text"},
+                ],
+            },
+            partial=True,
+        )
+
+        # trueのとき、is_valid()はvalidated_dataに格納する
+        assert serializer.is_valid()
+        serializer.save()
+
+        # post.refresh_from_db()
+        assert post.title == "new title"
+        assert post.content == "new content"
+        assert post.comments.count() == 1
+        assert post.comments.first().text == "new text"
--- app/test_models.py
+++ app/test_models.py
@@ -1,48 +1,40 @@
 import pytest
 from inline_snapshot import snapshot
 
-from app.models import (
-    Access,
-    ApiKeys,
-    Associate,
-    Data,
-    Space,
-    Structure,
-    User,
-)
+from app.models import Access, ApiKeys, Data, Organization, Space, User
 
 
 class TestApiKeysModel:
     @pytest.mark.django_db
     def test_正常系_Keyがユニークである(self):
-        api_key1 = ApiKeys.objects.create()
-        api_key2 = ApiKeys.objects.create()
-        assert api_key1.api_key != api_key2.api_key
+        key1 = ApiKeys.objects.create()
+        key2 = ApiKeys.objects.create()
+        assert key1.key != key2.key
 
     @pytest.mark.django_db
     def test_正常系_Keyが自動生成される(self):
-        api_key = ApiKeys.objects.create()
-        assert api_key.api_key is not None
+        key = ApiKeys.objects.create()
+        assert key.key is not None
 
     @pytest.mark.django_db
-    def test_正常系_関連するassociateのidを取得する(self):
-        associate = Associate.objects.create()
-        api_key = ApiKeys.objects.create(associate=associate)
-        assert api_key.associate.id == associate.id
+    def test_正常系_関連するorganizationのidを取得する(self):
+        space = Space.objects.create()
+        apikey = ApiKeys.objects.create(space=space)
+        assert apikey.space.id == space.id
 
     @pytest.mark.django_db
-    def test_異常系_関連するassociateが存在しない場合(self):
-        associate = Associate.objects.create()
-        api_key = ApiKeys.objects.create()
-        assert api_key.associate.id != associate.id
+    def test_異常系_関連するorganizationが存在しない場合(self):
+        space = Space.objects.create()
+        key = ApiKeys.objects.create()
+        assert key.space.id != space.id
 
 
 class TestAccessModel:
     @pytest.mark.django_db
-    def test_default_api_key(self):
-        api_key = ApiKeys.objects.create()
+    def test_default_key(self):
+        key = ApiKeys.objects.create()
         access = Access.objects.create()
-        assert access.api_key.id == api_key.id
+        assert access.api_key.id == key.id
 
     @pytest.mark.django_db
     def test_generate_new_api_key(self):
@@ -52,149 +44,12 @@ class TestAccessModel:
         assert access.api_key is not None
 
 
-class TestStructureModel:
-    @pytest.mark.django_db
-    def test_正常系_クエリが成功する場合(self):
-        """クエリが成功する場合"""
-        space = Space.objects.create()
-        structure = Structure.objects.create(space=space)
-        assert structure.space.id == space.id
-
-
 class TestSpaceModel:
     @pytest.mark.django_db
     def test_正常系_クエリが成功する場合(self):
         """クエリが成功する場合"""
         space = Space.objects.create()
         assert space.id is not None
-
-    @pytest.mark.django_db
-    def test_正常系_Structureモデルを取得する場合(self):
-        """Structureモデルを取得する場合"""
-        space = Space.objects.create()
-        structure = Structure.objects.create(space=space)
-        assert space.structures.first().id == structure.id
-
-    # structureが関連するspaceのためのstructure.idを返せているかを確認する
-    @pytest.fixture
-    def fixture_space(self):
-        # 複数のspaceを作成する
-        return [Space.objects.create() for _ in range(3)]
-
-    @pytest.fixture
-    def fixture_structure(self, fixture_space):
-        # spaceに関連するstructureを作成する
-        related_structure = [
-            Structure.objects.create(space=space) for space in fixture_space
-        ]
-        # 各spaceとは関連しないstructureを作成する
-        unrelated_structure = [Structure.objects.create() for _ in range(3)]
-
-        return related_structure, unrelated_structure
-
-    @pytest.mark.django_db
-    def test_正常系_関連するstructureを取得する場合(
-        self, fixture_space, fixture_structure
-    ):
-        """関連するstructureを取得する場合"""
-        related_structure, unrelated_structure = fixture_structure
-        for i, space in enumerate(fixture_space):
-            # logger.debug(f"### space: {space}")
-            # logger.debug(f"### related_structure[i]: {related_structure[i]}")
-            assert list(space.structures.all()) == [related_structure[i]]
-
-        for i, space in enumerate(fixture_space):
-            # logger.debug(f"### space: {space}")
-            # logger.debug(f"### unrelated_structure[i]: {unrelated_structure[i]}")
-            assert list(space.structures.all()) != [unrelated_structure[i]]
-
-    # 関連するstructureが複数ある場合に正しく取得できるかを確認する
-    @pytest.fixture
-    def fixture_related_structure(self, fixture_space: list[Space]) -> list[Structure]:
-        # spaceに関連するstructureを作成する
-        return [Structure.objects.create(space=space) for space in fixture_space]
-
-    @pytest.fixture
-    def fixture_related_structures(
-        self, fixture_space: list[Space]
-    ) -> list[list[Structure]]:
-        # 各spaceに関連する複数のstructureを作成する
-        structures = []
-        for space in fixture_space:
-            structures_for_space = [
-                Structure.objects.create(space=space) for _ in range(2)
-            ]  # 各Spaceに2つのStructureを関連付ける
-            structures.append(structures_for_space)
-        return structures
-
-    @pytest.mark.django_db
-    def test_正常系_関連するstructureが複数ある場合に正しく取得できるかを確認する(
-        self, fixture_space, fixture_related_structures
-    ):
-        """関連するstructureが複数ある場合に正しく取得できるかを確認する"""
-        for i, space in enumerate(fixture_space):
-            expected_structures = fixture_related_structures[i]
-            actual_structures = list(space.structures.all())
-            assert sorted(actual_structures, key=lambda s: s.id) == sorted(
-                expected_structures, key=lambda s: s.id
-            )
-
-    @pytest.fixture
-    def fixture_space_one(self) -> Space:
-        return Space.objects.create(id=9)
-
-    @pytest.fixture
-    def fixture_structures_for_space_one(
-        self, fixture_space_one: Space
-    ) -> list[Structure]:
-        structure_ids = [9, 8, 7]
-        return [
-            Structure.objects.create(id=id, space=fixture_space_one)
-            for id in structure_ids
-        ]
-
-    @pytest.mark.django_db
-    def test_正常系_関連するstructureが複数ある場合に正しく取得できるかを確認する_for_space_one(
-        self, fixture_space_one, fixture_structures_for_space_one
-    ):
-        """関連するstructureが複数ある場合に正しく取得できるかを確認する"""
-        expected_structure_ids = [9, 8, 7]
-        actual_structure_ids = [
-            structure.id for structure in fixture_space_one.structures.all()
-        ]
-        assert sorted(actual_structure_ids) == sorted(expected_structure_ids)
-
-
-class TestAssociateModel:
-    @pytest.mark.django_db
-    def test_正常系_クエリが成功する場合(self):
-        """クエリが成功する場合"""
-        associate = Associate.objects.create()
-        assert associate.id is not None
-
-    @pytest.mark.django_db
-    def test_正常系_関連するspaceとapi_keyを取得する場合(self):
-        """関連するspaceとapi_keyを取得する場合"""
-        associate = Associate.objects.create()
-        api_key = ApiKeys.objects.create(associate=associate)
-        assert associate.api_keys.first().id == api_key.id
-
-    @pytest.mark.django_db
-    def test_正常系_関連するspaceとapi_keyとaccessを取得する場合(self):
-        """関連するspaceとapi_keyとaccessを取得する場合
-        Associate
-           |
-           ApiKeys
-              |
-             Access
-                |
-              (access)
-        """
-        associate = Associate.objects.create()
-        api_key = ApiKeys.objects.create(associate=associate)
-        access = Access.objects.create(api_key=api_key)
-        assert associate.api_keys.first().access.first().id == access.id
-        assert associate.api_keys.first().access.first().api_key.id == api_key.id
 
 
 class TestUserModel:
@@ -205,58 +60,67 @@ class TestUserModel:
         assert user.id is not None
 
     @pytest.mark.django_db
-    def test_正常系_関連するassociateを取得する場合(self):
-        """関連するassociateを取得する場合"""
+    def test_正常系_関連するorganizationを取得する場合(self):
+        """関連するorganizationを取得する場合"""
         user = User.objects.create()
-        associate = Associate.objects.create()
-        user.associates.add(associate)
-        assert user.associates.first().id == associate.id
+        organization = Organization.objects.create()
+        user.organization.add(organization)
+        assert user.organization.first().id == organization.id
 
     @pytest.mark.django_db
-    def test_正常系_関連するassociateとspaceとapi_keyを取得する場合(self):
-        """関連するassociateとspaceとapi_keyを取得する場合
-        user[1, 2]が同じassociate[1]に所属している
+    def test_正常系_関連するorganizationとspaceとapikeyを取得する場合(self):
+        """関連するorganizationとapi_keyを取得する場合
+        user[1, 2]が同じorganization[1]に所属している
         """
         user1 = User.objects.create()
         user2 = User.objects.create()
-        associate = Associate.objects.create()
-        api_key = ApiKeys.objects.create(associate=associate)
-        user1.associates.add(associate)
-        user2.associates.add(associate)
-        # assert (
-        #     user1.associates.first().spaces.first().api_keys.first().id
-        #     == user2.associates.first().spaces.first().api_keys.first().id
-        # )
+        organization = Organization.objects.create()
+        user1.organization.add(organization)
+        user2.organization.add(organization)
+        space = Space.objects.create(organization=organization)
+        apikey = ApiKeys.objects.create(space=space)
+
         assert (
-            user1.associates.first().api_keys.first().id
-            == user2.associates.first().api_keys.first().id
+            user1.organization.first().spaces.first().api_keys.first().id
+            == user2.organization.first().spaces.first().api_keys.first().id
         )
 
     @pytest.mark.django_db
-    def test_正常系_関連するassociateとspaceとapi_keyを取得する場合(self):
-        """関連するassociateとspaceとapi_keyを取得する場合
-        user[1, 2]が同じassociate[1]に所属している
-        user[3]は異なるassociate[2]に所属している
+    def test_正常系_関連するorganizationとspaceとapi_keyを取得する場合(self):
+        """関連するorganizationとspaceとapi_keyを取得する場合
+        user[1, 2]が同じorganization[1]に所属している
+        user[3]は異なるorganization[2]に所属している
         """
         user1 = User.objects.create()
         user2 = User.objects.create()
         user3 = User.objects.create()
-        associate1 = Associate.objects.create()
-        associate2 = Associate.objects.create()
-        api_key1 = ApiKeys.objects.create(associate=associate1)
-        api_key2 = ApiKeys.objects.create(associate=associate2)
-        user1.associates.add(associate1)
-        user2.associates.add(associate1)
-        user3.associates.add(associate2)
+        organization1 = Organization.objects.create()
+        organization2 = Organization.objects.create()
+        space1 = Space.objects.create(organization=organization1)
+        space2 = Space.objects.create(organization=organization2)
+        api_key1 = ApiKeys.objects.create(space=space1)
+        api_key2 = ApiKeys.objects.create(space=space2)
+        user1.organization.add(organization1)
+        user2.organization.add(organization1)
+        user3.organization.add(organization2)
 
-        # user1とuser2が同じassociate1に所属していることを検証
-        assert user1.associates.first().api_keys.first().id == api_key1.id
-        assert user2.associates.first().api_keys.first().id == api_key1.id
+        # user1とuser2が同じorganization1に所属していることを検証
+        assert (
+            user1.organization.first().spaces.first().api_keys.first().id == api_key1.id
+        )
+        assert (
+            user2.organization.first().spaces.first().api_keys.first().id == api_key1.id
+        )
 
-        # user3が異なるassociate2に所属していることを検証
-        assert user3.associates.first().id == associate2.id
-        assert not user3.associates.first().api_keys.first().id == api_key1.id
-        assert user3.associates.first().api_keys.first().id == api_key2.id
+        # user3が異なるorganization2に所属していることを検証
+        assert user3.organization.first().id == organization2.id
+        assert (
+            not user3.organization.first().spaces.first().api_keys.first().id
+            == api_key1.id
+        )
+        assert (
+            user3.organization.first().spaces.first().api_keys.first().id == api_key2.id
+        )
 
 
 class TestDataModel:
